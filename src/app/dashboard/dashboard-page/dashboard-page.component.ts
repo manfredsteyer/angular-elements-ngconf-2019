e2eebd9bb6aa7b32edcae4a05e8dc6f8a139cd41
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-07-05 22:55:13 +0200

    lazy-dashboard removed

--- src/app/dashboard/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/dashboard-page/dashboard-page.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ExternalDashboardTileService } from './external-dashboard-tile.service';
-import { LazyDashboardTileService } from './lazy-dashboard-tile.service';
+import {Component, OnInit} from '@angular/core';
+import {ExternalDashboardTileService} from './external-dashboard-tile.service';
 
 @Component({
   templateUrl: './dashboard-page.component.html',
@@ -9,9 +8,9 @@ import { LazyDashboardTileService } from './lazy-dashboard-tile.service';
 export class DashboardPageComponent {
 
   constructor(
-    private lazyService: LazyDashboardTileService,
     private externalService: ExternalDashboardTileService
-  ) { }
+  ) {
+  }
 
 
   addTile(): void {
@@ -20,7 +19,7 @@ export class DashboardPageComponent {
 
   private _add(tileKind: string): void {
 
-    const data =  [
+    const data = [
       Math.round(Math.random() * 100),
       Math.round(Math.random() * 100),
       Math.round(Math.random() * 100)
@@ -44,9 +43,7 @@ export class DashboardPageComponent {
     // tslint:disable-next-line:no-console
     console.debug(`This exercise does not run in StackBlitz, hence it is part of the director's cut of this exercise`);
 
-    this.lazyService.load().then(_ => {
-      this._add('lazy-dashboard-tile');
-    });
+
   }
 
   addExternal(): void {
