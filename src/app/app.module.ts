2c3ff1666fed586f0c0c6f587923977dee3a091a
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-07-05 22:48:18 +0200

    shared component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import {AppComponent} from './app.component';
 import {APP_ROUTES} from './app.routes';
 import {HomeComponent} from './home/home.component';
 import {NavbarComponent} from './navbar/navbar.component';
-import {SharedModule} from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardTileModule } from './dashboard/dashboard-tile/dashboard-tile.module';
 import { TabsPageComponent } from './tab-example/tabs-page/tabs-page.component';
@@ -22,7 +21,6 @@ import { CustomCheckboxModule } from './native-elements/custom-checkbox/custom-c
       BrowserModule,
       BrowserAnimationsModule,
       HttpClientModule,
-      SharedModule.forRoot(),
       RouterModule.forRoot(APP_ROUTES),
       DashboardTileModule,
       TabsModule,
