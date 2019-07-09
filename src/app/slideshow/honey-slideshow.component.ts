821f7a6047d83e26aed713cf0b9acac50486ef61
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-07-09 21:26:07 +0200

    erster Durchstich realisiert

--- src/app/slideshow/honey-slideshow.component.ts
+++ src/app/slideshow/honey-slideshow.component.ts
@@ -1,22 +1,54 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {AfterViewInit, Component, Input, Output} from '@angular/core';
+import {parse} from 'marked';
+
+
+interface Slide {
+
+  slideURL;
+
+}
+
 
 @Component({
   selector: 'app-honey-slideshow',
   templateUrl: './honey-slideshow.component.html'
 })
-export class HoneySlideshowComponent implements OnInit {
-  @Input() slideURL: string;
-  @Input() slideTitle: string;
+export class HoneySlideshowComponent implements AfterViewInit {
+
+  /**
+   * [
+   * {
+   *    "slideURL": "url des slide",
+   *    "audioURL": "url des audio"
+   * }
+   * ]
+   */
+  @Input() slides: Slide[];
+
+
+  public compute = ($event) => {
+    // console.log(`slideContent${this.slides[0]['slideURL']}`);
+    const fetchedData: Promise<string> = fetch(this.slides[0].slideURL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.text() as Promise<string>;
+      })
+      .then((data: string) => {
+        const text = parse(data);
+        console.log('###Text:>' + text + '<');
+        const element: HTMLElement = document.getElementById('slide-frame');
+        element.innerHTML = text;
+        return data;
+      });
+  };
 
   // data for chart
-  slides: object = {};
+  // slides: object = {};
 
-  ngOnInit() {
-    this.slides = [
-      {
-        url: this.slideURL,
-        title: this.slideTitle
-      }
-    ];
+  ngAfterViewInit() {
+
+
   }
 }
