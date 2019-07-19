e1fcabfcf49c1f681b00a33488b5d47d24e45d5c
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-07-19 23:37:19 +0200

    Axe Tests added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,8 @@
 import {AppPage} from './app.po';
 import {browser, logging} from 'protractor';
 import * as axe from 'axe-core';
+import * as assert from 'assert';
+import {AxeResults} from 'axe-core';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -14,9 +16,25 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('Präsentation');
   });
 
-  xit('prüfe Barrierefreiheit', () => {
+  it('prüfe Barrierefreiheit', async () => {
     page.navigateTo();
-    // browser.executeScript(axe.source);
+    browser.executeScript(axe.source);
+
+
+    // @ts-ignore
+    const result: AxeResults = await browser.executeAsyncScript((resolve: any) => {
+
+      axe.run({exclude: []}, {}, (error, results) => {
+        if (error) {
+          throw error;
+        }
+        resolve(results);
+      });
+    }) as Promise<AxeResults>;
+
+    assert.equal(result.violations.length, 0,
+      `Axe: Folgende ${result.violations.length} Fehler gefunden: ${JSON.stringify(result.violations, null, 1)}`
+    );
 
   });
 
