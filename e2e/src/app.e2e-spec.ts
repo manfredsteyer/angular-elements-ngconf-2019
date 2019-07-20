407918016d68625cd24df53b38608bdc4a3bb8d7
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-07-20 15:18:34 +0200

    Axe Prüfung nach Interaction eingebaut

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,9 +3,43 @@ import {browser, logging} from 'protractor';
 import * as axe from 'axe-core';
 import * as assert from 'assert';
 import {AxeResults} from 'axe-core';
+import {throws} from 'assert';
+import any = jasmine.any;
 
 describe('workspace-project App', () => {
   let page: AppPage;
+
+  async function printAxeRuleSet() {
+
+    // @ts-ignore
+    const ruleSet: string = await browser.executeAsyncScript((resolve: any) => {
+      resolve(JSON.stringify(axe.getRules(), null, 1));
+    }) as Promise<AxeResults>;
+
+    console.log('### Ruleset:' + ruleSet);
+  }
+
+  function prepareAxeTests() {
+    // Auf ausstehende ticks warten
+    browser.pause(50);
+    browser.executeScript(axe.source);
+  }
+
+  async function pruefeBarrierefreiheit(): Promise<AxeResults> {
+
+    // @ts-ignore
+    const result: AxeResults = await browser.executeAsyncScript((resolve: any) => {
+      axe.run({exclude: []}, {}, (error, results) => {
+        if (error) {
+          throw error;
+        }
+        resolve(results);
+      });
+    }) as Promise<AxeResults>;
+
+    return result;
+  }
+
 
   beforeEach(() => {
     page = new AppPage();
@@ -16,24 +50,26 @@ describe('workspace-project App', () => {
     expect(page.getTitleText()).toEqual('Präsentation');
   });
 
-  it('prüfe Barrierefreiheit', async () => {
+  it('Prüfe Barrierefreiheit im default Zustand', async () => {
+
     page.navigateTo();
-    browser.executeScript(axe.source);
+    prepareAxeTests();
+    // printAxeRuleSet();
+    const results = await pruefeBarrierefreiheit();
+    assert.equal(results.violations.length, 0,
+      `Axe: Folgende ${results.violations.length} Fehler gefunden: ${JSON.stringify(results.violations, null, 1)}`
+    );
+  });
 
-
-    // @ts-ignore
-    const result: AxeResults = await browser.executeAsyncScript((resolve: any) => {
-
-      axe.run({exclude: []}, {}, (error, results) => {
-        if (error) {
-          throw error;
-        }
-        resolve(results);
-      });
-    }) as Promise<AxeResults>;
-
-    assert.equal(result.violations.length, 0,
-      `Axe: Folgende ${result.violations.length} Fehler gefunden: ${JSON.stringify(result.violations, null, 1)}`
+  it('Prüfe Barrierefreiheit nach Aktion', async () => {
+    page.navigateTo();
+    await page.clickPraesentationButton();
+    // browser.pause(500);
+    prepareAxeTests();
+    // printAxeRuleSet();
+    const results = await pruefeBarrierefreiheit();
+    assert.equal(results.violations.length, 0,
+      `Axe: Folgende ${results.violations.length} Fehler gefunden: ${JSON.stringify(results.violations, null, 1)}`
     );
 
   });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,18 @@
-import {browser, by, element} from 'protractor';
+import {browser, by, element, ElementFinder} from 'protractor';
 
 export class AppPage {
+
+  praesentationButton: ElementFinder = element(by.id('start-praesentation'));
+
   navigateTo(path: string = '/') {
     return browser.get(browser.baseUrl + path) as Promise<any>;
   }
 
   getTitleText() {
     return element(by.id('titel')).getText() as Promise<string>;
+  }
+
+  async clickPraesentationButton() {
+    return this.praesentationButton.click();
   }
 }
